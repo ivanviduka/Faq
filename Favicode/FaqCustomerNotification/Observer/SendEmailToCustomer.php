ae3e73f28689b355c4f220827b74ccdd2ca59e70
ivanviduka <viduka.ivan@gmail.com>
2022-08-22 09:23:41 +0200

    Email is not sent to the customer when FAQ status is changed

--- Favicode/FaqCustomerNotification/Observer/SendEmailToCustomer.php
+++ Favicode/FaqCustomerNotification/Observer/SendEmailToCustomer.php
@@ -5,6 +5,7 @@ namespace Favicode\FaqCustomerNotification\Observer;
 
 use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Escaper;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
@@ -57,12 +58,18 @@ class SendEmailToCustomer implements ObserverInterface
      */
     private $customerRepository;
 
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
     public function __construct(
         TransportBuilder $transportBuilder,
         StateInterface $inlineTranslation,
         StoreManagerInterface $storeManager,
         CustomerRepositoryInterface $customerRepository,
         ScopeConfigInterface $scopeConfig,
+        RequestInterface $request,
         Escaper $escaper,
         LoggerInterface $logger
     ) {
@@ -73,6 +80,7 @@ class SendEmailToCustomer implements ObserverInterface
         $this->_escaper = $escaper;
         $this->logger = $logger;
         $this->customerRepository = $customerRepository;
+        $this->request = $request;
     }
 
     /**
@@ -83,44 +91,46 @@ class SendEmailToCustomer implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        $question = $observer->getData('question');
-        $questionText = $question->getQuestionText();
-        $questionAnswer = $question->getQuestionAnswer();
-        $customer = $this->customerRepository->getById($question->getCustomerId());
-        $username = $customer->getFirstname() . " " . $customer->getLastname();
+        if($this->request->getActionName() == 'save'){
+            $question = $observer->getData('question');
+            $questionText = $question->getQuestionText();
+            $questionAnswer = $question->getQuestionAnswer();
+            $customer = $this->customerRepository->getById($question->getCustomerId());
+            $username = $customer->getFirstname() . " " . $customer->getLastname();
 
-        $storeScope = ScopeInterface::SCOPE_STORE;
+            $storeScope = ScopeInterface::SCOPE_STORE;
 
-        try {
-            $sender = [
-                'name' => $this->scopeConfig->getValue(self::XML_PATH_NAME_ADMIN, $storeScope),
-                'email' => $this->scopeConfig->getValue(self::XML_PATH_EMAIL_ADMIN, $storeScope)
-            ];
+            try {
+                $sender = [
+                    'name' => $this->scopeConfig->getValue(self::XML_PATH_NAME_ADMIN, $storeScope),
+                    'email' => $this->scopeConfig->getValue(self::XML_PATH_EMAIL_ADMIN, $storeScope)
+                ];
 
-            $templateOptions = [
-                'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
-                'store' => $this->storeManager->getStore()->getId()
-            ];
-            $templateVars = [
-                'user_name' => $this->_escaper->escapeHtml($username),
-                'question_text' => $this->_escaper->escapeHtml($questionText),
-                'question_answer' => $this->_escaper->escapeHtml($questionAnswer)
-            ];
+                $templateOptions = [
+                    'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+                    'store' => $this->storeManager->getStore()->getId()
+                ];
+                $templateVars = [
+                    'user_name' => $this->_escaper->escapeHtml($username),
+                    'question_text' => $this->_escaper->escapeHtml($questionText),
+                    'question_answer' => $this->_escaper->escapeHtml($questionAnswer)
+                ];
 
-            $this->inlineTranslation->suspend();
+                $this->inlineTranslation->suspend();
 
-            $transport = $this->_transportBuilder->setTemplateIdentifier(self::EMAIL_TEMPLATE)
-                ->setTemplateOptions($templateOptions)
-                ->setTemplateVars($templateVars)
-                ->setFromByScope($sender, $storeScope)
-                ->addTo($customer->getEmail())
-                ->getTransport();
+                $transport = $this->_transportBuilder->setTemplateIdentifier(self::EMAIL_TEMPLATE)
+                    ->setTemplateOptions($templateOptions)
+                    ->setTemplateVars($templateVars)
+                    ->setFromByScope($sender, $storeScope)
+                    ->addTo($customer->getEmail())
+                    ->getTransport();
 
-            $transport->sendMessage();
-            $this->inlineTranslation->resume();
+                $transport->sendMessage();
+                $this->inlineTranslation->resume();
 
-        } catch (\Exception $e) {
-            $this->logger->error($e->getMessage());
+            } catch (\Exception $e) {
+                $this->logger->error($e->getMessage());
+            }
         }
     }
 }
